accec6bc52f34fd4caeb1fa359142864c360115b
Rutuja Sanjay Jotrao <95583405+RutujaJotrao@users.noreply.github.com>
2022-10-08 17:37:13 +0530

    Time: 11 ms (42.50%), Space: 9 MB (67.69%) - LeetHub

---
+++ 443-string-compression/443-string-compression.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;    
+        int ans=0;  //valid char stored in it
+        int n=chars.size();
+        while(i<n)
+        {
+            int j=i+1;
+            while(j<n && chars[i]==chars[j])
+            {
+                j++;
+            }
+            //vector traversed 
+            chars[ans++]=chars[i];
+            int count=j-i;
+            if(count>1)
+            {
+                string cnt=to_string(count);
+                for(char ch:cnt)
+                {
+                    chars[ans++]=ch;
+                }
+            }
+            i=j;
+        }
+        return ans;
+    }
+};
